402752888c703e01c2ddbb6fe249d0830eb964ed
hezron <hezzykialo.official.ke@gmail.com>
2022-06-03 20:04:49 +0300

    create the logic once user is login can view a profile and logout,createsuperuser(instahezron)

--- instagra_app/urls.py
+++ instagra_app/urls.py
@@ -5,6 +5,7 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('',views.home,name='home'),
     path('register/',views.register,name='register'),
+    path('profile/',views.profile,name='profile'),
     path('login/',auth_views.LoginView.as_view(template_name='auth/login.html'), name='login'),
     path('login/',auth_views.LogoutView.as_view(template_name='auth/logout.html'), name='logout'),
 ]
--- instagra_app/views.py
+++ instagra_app/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render,redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 from .forms import UserRegisterForm
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def home(request):
@@ -27,3 +28,7 @@ def register(request):
 
     return render(request, 'auth/register.html',ctx)
 
+@login_required
+def profile(request):
+    
+    return render(request, 'auth/profile.html')
